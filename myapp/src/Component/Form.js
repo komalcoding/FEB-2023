6bf25cdea8b28766219a1527e520578275bcd1fd
komalcoding <komalcoding@gmail.com>
2023-02-26 09:47:46 +0530

    added

---
+++ myapp/src/Component/Form.js
@@ -0,0 +1,16 @@
+function Form(){
+    return <div>
+      <div>
+        <form>
+        <label for="name">name</label>
+        <input type="text" name="name" id="name"></input><br></br>
+        <label for="name">location</label>
+        <input type="text" name="location" id="location"></input><br/>
+        <label for="name">description</label>
+        <input type="text" name="description" id="des"></input>
+        
+        </form>
+      </div>
+    </div>
+}
+export default Form
